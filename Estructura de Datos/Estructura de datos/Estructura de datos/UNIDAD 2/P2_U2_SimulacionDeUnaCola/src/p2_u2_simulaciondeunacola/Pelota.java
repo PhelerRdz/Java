720f77e52c6ff9e72b925efe42b7f62a801341cf
Jorge Rodriguez <jorgerdz2806@gmail.com>
2022-04-30 19:26:59 -0500

    a

---
+++ Estructura de Datos/Estructura de datos/Estructura de datos/UNIDAD 2/P2_U2_SimulacionDeUnaCola/src/p2_u2_simulaciondeunacola/Pelota.java
@@ -0,0 +1,7 @@
+/**@author 2183330170 Zubiri Valdez Hedson Leonardo Zubiri*/
+package p2_u2_simulaciondeunacola;
+
+public class Pelota {
+    String color;
+    int cantidad;
+}
---
+++ Estructura de Datos/Estructura de datos/Estructura de datos/UNIDAD 2/P3_U2_SimulacionColaPrioridad/src/p3_u2_simulacioncolaprioridad/Avion.java
@@ -0,0 +1,7 @@
+/**@author 2183330170 Zubiri Valdez Hedson Leonardo Zubiri*/
+package p3_u2_simulacioncolaprioridad;
+
+public class Avion {
+    String codigoVuelo;
+    String company;
+}
