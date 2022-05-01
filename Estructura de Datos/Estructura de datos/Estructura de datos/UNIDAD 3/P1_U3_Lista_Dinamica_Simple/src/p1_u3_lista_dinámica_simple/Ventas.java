5fd89ebc047acccbcc5bbcc1d1238c5d4157ecfd
Jorge Rodriguez <jorgerdz2806@gmail.com>
2022-04-30 19:26:34 -0500

    a

---
+++ Estructura de Datos/Estructura de datos/Estructura de datos/UNIDAD 3/P1_U3_Lista_Dinamica_Simple/src/p1_u3_lista_dinámica_simple/Ventas.java
@@ -0,0 +1,11 @@
+/**@author 2183330170 Zubiri Valdez Hedson Leonardo Zubiri*/
+package p1_u3_lista_dinámica_simple;
+
+public class Ventas {
+    int clave;
+    int cantidad;
+    double precio;
+    String descripcion;
+    
+    Ventas siguiente=null;
+}
---
+++ Estructura de Datos/Estructura de datos/Estructura de datos/UNIDAD 3/P2_U3_ListaDobleEnlace_Pila/src/p2_u3_listadobleenlace_pila/Alumno.java
@@ -0,0 +1,11 @@
+/**@author 2183330170 Zubiri Valdez Hedson Leonardo Zubiri*/
+package p2_u3_listadobleenlace_pila;
+
+public class Alumno {
+    int clave;
+    String nombre;
+    double evaluacion;
+    
+    Alumno anterior=null;
+    Alumno siguiente=null;
+}
