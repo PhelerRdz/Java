3994aba0e36ad158a9c73827a27eab30170879a0
Jorge Rodriguez <jorgerdz2806@gmail.com>
2022-04-30 19:27:18 -0500

    a

---
+++ Estructura de Datos/Estructura de datos/Estructura de datos/UNIDAD 1/P7_U1_MetodoBusquedaSecuencial/src/p7_u1_metodobusquedasecuencial/Auto.java
@@ -0,0 +1,8 @@
+/**@author 2183330170 Zubiri Valdez Hedson Leonardo Zubiri*/
+package p7_u1_metodobusquedasecuencial;
+//Estructura
+public class Auto {
+    String color;
+    int NoAsientos;
+    String modelo;
+}
