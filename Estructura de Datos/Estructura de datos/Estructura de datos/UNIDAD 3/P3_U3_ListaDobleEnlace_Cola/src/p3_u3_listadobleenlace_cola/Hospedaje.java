b8359628393ec30f99087631cecad140dfd05b6c
Jorge Rodriguez <jorgerdz2806@gmail.com>
2022-04-30 19:26:12 -0500

    a

---
+++ Estructura de Datos/Estructura de datos/Estructura de datos/UNIDAD 3/P3_U3_ListaDobleEnlace_Cola/src/p3_u3_listadobleenlace_cola/Hospedaje.java
@@ -0,0 +1,10 @@
+/**@author 2183330170 Zubiri Valdez Hedson Leonardo Zubiri*/
+package p3_u3_listadobleenlace_cola;
+//Estructura Hospedaje
+public class Hospedaje {
+    String clave;
+    String desc;
+    String integrantes;
+    
+    Hospedaje ant,sig;
+}
